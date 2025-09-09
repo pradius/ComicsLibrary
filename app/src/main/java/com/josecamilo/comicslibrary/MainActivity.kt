e010425654c8c33ea9e6e22bbe03383ee117b4ee
josecamilo <josecamilo@outlook.com>
2025-09-08 20:05:22 -0400

    added base routes

--- app/src/main/java/com/josecamilo/comicslibrary/MainActivity.kt
+++ app/src/main/java/com/josecamilo/comicslibrary/MainActivity.kt
@@ -6,24 +6,40 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.josecamilo.comicslibrary.ui.theme.ComicsLibraryTheme
+import com.josecamilo.comicslibrary.view.CharacterDetailScreen
+import com.josecamilo.comicslibrary.view.CollectionScreen
+import com.josecamilo.comicslibrary.view.LibraryScreen
 
+sealed class Destination(val route: String) {
+    object Library : Destination("library")
+    object Collection : Destination("collection")
+    object CharacterDetail : Destination("character_detail/{characterId}") {
+        fun createRoute(characterId: Int?) = "character_detail/$characterId"
+    }
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComicsLibraryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    CharactersScaffold(navController = navController)
                 }
             }
         }
@@ -31,17 +47,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun CharactersScaffold(navController: NavHostController) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComicsLibraryTheme {
-        Greeting("Android")
+    Scaffold(
+        bottomBar = {}
+    ) { paddingValues ->
+        Text("Hello World", modifier = Modifier.padding(paddingValues))
+        NavHost(navController, startDestination = Destination.Library.route) {
+            composable(Destination.Library.route) {
+                LibraryScreen()
+            }
+            composable(Destination.Collection.route){
+                CollectionScreen()
+            }
+            composable(Destination.CharacterDetail.route) { navBackStackEntry ->
+                val characterId = navBackStackEntry.arguments?.getString("characterId")?.toIntOrNull()
+                CharacterDetailScreen(characterId = characterId ?: 0)
+            }
+        }
     }
 }
